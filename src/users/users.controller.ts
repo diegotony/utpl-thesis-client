b0e0fbee315ee3607ba886263edd19432e2338fd
diegotony <tucotony1396@gmail.com>
2019-10-09 20:13:36 -0500

    swagger fixed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { MessagePattern } from '@nestjs/microservices';
 import { CreateUserDto } from '../../shared/dto/create-user.dto';
 import { UsersService } from './users.service';
 import { User } from '../../shared/dto/user.dto';
+import { ApiImplicitBody, ApiImplicitParam } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
@@ -11,7 +12,7 @@ export class UsersController {
     @Post()
     @HttpCode(204)
     async create(@Body() dto: CreateUserDto) {
-      // TODO: map to UserDTO
+
       return (await this.usersService.createUser(dto));
     }
 
@@ -24,26 +25,29 @@ export class UsersController {
         first_name: v.first_name, last_name: v.last_name, email: v.email, dni: v.dni,
       }));
     }
-
     @Get(':id')
+    @ApiImplicitParam({ name: 'id' })
     @HttpCode(200)
     async findUser(@Param() params): Promise<User[]> {
       return (await this.usersService.findUser(params.id));
     }
 
     @Put(':id')
+    @ApiImplicitParam({ name: 'id' })   
+    @HttpCode(204)
     async editUser(@Param() params, @Body() dto:CreateUserDto){
       return (await this.usersService.editUser(params.id,dto));
     }
 
-    @Put(':id')
+    @Delete(':id')
+    @ApiImplicitParam({ name: 'id' })
     async deleteUser(@Param() params): Promise<User[]> {
       return (await this.usersService.deleteUser(params.id));
     }
 
-    @MessagePattern({ cmd: 'compareHash' })
-    async compareHash(password, hash): Promise<Boolean> {
-      return (await this.usersService.compareHash(password, hash));
-    }
+    // @MessagePattern({ cmd: 'compareHash' })
+    // async compareHash(password, hash): Promise<Boolean> {
+    //   return (await this.usersService.compareHash(password, hash));
+    // }
 
   }
