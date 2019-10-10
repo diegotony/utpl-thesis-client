6dc22ed179a3de66ffadb1420d83819d9a1cb37a
diegotony <tucotony1396@gmail.com>
2019-10-10 01:18:50 -0500

    testing

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import {UserSchema} from './../schemas/user.schema';
+import { User } from 'shared/dto/user.dto';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, {
+    provide: getModelToken('User'),
+    useValue: User,
+  },],
 })
 export class UsersModule {}
