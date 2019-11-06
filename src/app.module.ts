d480be7a2e127651d96a92a2d79cd33957840678
diegotony <tucotony1396@gmail.com>
2019-11-06 18:09:47 -0500

    terminus

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UsersModule } from "./users/users.module";
+import { TerminusOptionsService } from "./services/terminus-options/terminus-options.service";
 import config from "./config/config";
+import { TerminusModule } from "@nestjs/terminus";
 @Module({
   imports: [
     MongooseModule.forRoot(
       `mongodb://${config.MONGO_HOST}:${config.MONGO_PORT}/${config.MONGO_DB}`
     ),
-    UsersModule
+    UsersModule,
+    TerminusModule.forRootAsync({useClass: TerminusOptionsService})
   ],
-  controllers: []
+  controllers: [],
+  providers: [TerminusOptionsService]
 })
 export class AppModule {}
