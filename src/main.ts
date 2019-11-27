4004782d7f8b97358e93015a506480e09f7a5643
tony <tony@kakashi>
2019-11-27 01:37:27 -0500

    redis depdnedency

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,9 @@ import {OPTIONS} from './main-optinos';
 async function bootstrap() {
 
   // const redis = await NestFactory.createMicroservice(AppModule, OPTIONS);
-
-
   // redis.listen(() => console.log('Microservice is listening'));
-  const app = await NestFactory.create(AppModule);
 
+  const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
   .setTitle('User Service')
   .setDescription('The service API description')
@@ -20,7 +18,7 @@ async function bootstrap() {
   .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api/users', app, document);
-
   await app.listen(config.PORT);
+  
 }
 bootstrap();
